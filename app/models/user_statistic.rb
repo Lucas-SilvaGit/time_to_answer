c5afa7948ca40e9c6f2046104738a5526ad0d1aa
Lucas-SilvaGit <lucasnba2324@gmail.com>
2022-09-12 06:54:08 -0300

    configuration to search answer in redis

--- app/models/user_statistic.rb
+++ app/models/user_statistic.rb
@@ -7,10 +7,10 @@ class UserStatistic < ApplicationRecord
   end
 
   # Class Methods
-  def self.set_statistic(answer, current_user)
+  def self.set_statistic(correct, current_user)
     if !!current_user
       user_statistic = UserStatistic.find_or_create_by(user: current_user)
-      if answer.correct?
+      if correct
         user_statistic.right_questions += 1
       else
         user_statistic.wrong_questions += 1
